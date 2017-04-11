c82207267e20bab47b58198802b50bf8d1090d90
Bodestad <gustav.bodestad@gmail.com>
2017-04-11 13:21:04 +0200

    Gustavs first commit

--- src/Database.java
+++ src/Database.java
@@ -2,4 +2,8 @@
  * Created by Johan on 2017-04-11.
  */
 public class Database {
+    public static void main(String[] args) {
+        System.out.println("Paulo Roberto kan fara åt helvete");
+    }
+
 }
