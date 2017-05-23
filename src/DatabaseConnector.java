1e2d3042eeac040219fd450b49b58e7a62203162
JohanJohansson93 <johansson.johan93@gmail.com>
2017-05-23 10:31:56 +0200

    Files updated

--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -2,7 +2,5 @@
  * Created by Johan on 2017-04-11.
  */
 public class DatabaseConnector {
-    public static void main(String[] args) {
-        System.out.println("Paulo Roberto kan fara åt helvete ¯\\_(ツ)_/¯");
-    }
+
 }
--- src/Product.java
+++ src/Product.java
@@ -2,4 +2,56 @@
  * Created by Johan on 2017-05-23.
  */
 public class Product {
+
+    private String Name;
+    private double pounds, dollars, crowns;
+    private int stockID;
+
+    public Product(String name, double PriceinPounds, double PriceinDollars, double PriceinCrowns, int ID){
+        this.Name = name;
+        this.pounds = PriceinPounds;
+        this.dollars = PriceinDollars;
+        this.crowns = PriceinCrowns;
+        this.stockID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public double getPounds() {
+        return pounds;
+    }
+
+    public void setPounds(double pounds) {
+        this.pounds = pounds;
+    }
+
+    public double getDollars() {
+        return dollars;
+    }
+
+    public void setDollars(double dollars) {
+        this.dollars = dollars;
+    }
+
+    public double getCrowns() {
+        return crowns;
+    }
+
+    public void setCrowns(double crowns) {
+        this.crowns = crowns;
+    }
+
+    public int getStockID() {
+        return stockID;
+    }
+
+    public void setStockID(int stockID) {
+        this.stockID = stockID;
+    }
 }
