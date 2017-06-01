a6d8d6e12e849e32d10c25c91cff7b2df1f76dad
Lorenz Puskas <lorenzpuskas@hotmail.com>
2017-06-01 14:53:31 +0200

    report updated

--- src/Report.java
+++ src/Report.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 /**
  * Created by Johan on 2017-05-23.
  */
+
+
 public class Report {
+    public static List<Date> getDaysBetweenDates(Date startdate, Date enddate) {
+
+        // En början iaf...
+        List<Date> dates = new ArrayList<Date>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(startdate);
+
+        while (calendar.getTime().before(enddate))
+        {
+            Date result = calendar.getTime();
+            dates.add(result);
+            calendar.add(Calendar.DATE, 1);
+        }
+        return dates;
+    }
+
 }
+
